ec93da0a82eff7c1d69b1f63555fb08b74ffb8dd
Nguyễn Thanh Long <longnt@ftech.ai>
2020-11-20 17:22:51 +0700

    Add stuff

--- Assets/Scripts/Managers/GamePlayManager.cs
+++ Assets/Scripts/Managers/GamePlayManager.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class GamePlayManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Application.targetFrameRate = 60;
     }
 }
